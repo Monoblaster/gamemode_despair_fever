ab3f2bedc3fd8c05d0284af35950774fa67331d1
Monoblaster <64797269+Monoblaster@users.noreply.github.com>
2022-02-14 13:30:28 -0800

    init

--- lib/noObservers.cs
+++
@@ -1,7 +0,0 @@
-package NoObserversPackage
-{
-  function SimObject::onCameraEnterOrbit(%obj, %camera) {}
-  function SimObject::onCameraLeaveOrbit(%obj, %camera) {}
-};
-
-activatePackage("NoObserversPackage");
