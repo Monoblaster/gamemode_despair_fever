b77bfb88a8dc5f45142caee40e0fb6986593b41f
Monoblaster <64797269+Monoblaster@users.noreply.github.com>
2022-02-23 13:56:39 -0800

    inventory

---
+++ lib/inventory.cs
@@ -0,0 +1,9 @@
+//generic way to handle new inventory system
+//items have a slot number
+//1 takes up 1 slot and so on
+//slots have a similar number
+//slots also define slots they are connected to
+//slots are objects that hold the items within them
+//the default inventory system is not used server side
+//but the server will send slot information to the client
+//slots 
